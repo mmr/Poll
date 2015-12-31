827b60e24833f0d43ece601d0417d71d5fc760a2
Marcio Ribeiro <marcio@loggi.com>
2015-12-31 15:44:11 -0200

    [add] Initial working version with map and button

---
+++ index.ios.js
@@ -0,0 +1,94 @@
+/**
+ * @flow
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  MapView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} = React;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    height: 450,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  mapContainer: {
+    height: 450,
+    margin: 5,
+    marginTop: 20,
+    backgroundColor: '#eaeaea',
+  },
+  buttonPressed: {
+    flex: 1,
+    height: 60,
+    backgroundColor: 'gray',
+  },
+  buttonNotPressed: {
+    flex: 1,
+    height: 40,
+    backgroundColor: 'gray',
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white'
+  },
+});
+
+var PollButton = React.createClass({
+  getInitialState: function() {
+    return {
+      toggled: false
+    };
+  },
+
+  _onPress: function() {
+    this.setState({
+      toggled: !this.state.toggled
+    });
+  },
+
+  render: function() {
+    var buttonStyle = this.state.toggled ? styles.buttonPressed : styles.buttonNotPressed;
+
+    return (
+      <TouchableHighlight onPress={this._onPress}>
+        <View style={buttonStyle}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              Poll
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  },
+});
+
+var Poll = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.mapContainer}>
+          <MapView style={styles.map} showsUserLocation={true} />
+        </View>
+        <PollButton />
+      </View>
+    );
+  }
+});
+
+AppRegistry.registerComponent('Poll', () => Poll);
